b559e4297d12b2af5f8dcc2386d7e8d0a78eb751
Gautam <61959145+gautam7-github@users.noreply.github.com>
2022-04-30 13:19:51 +0530

    added console.log feature

--- contract.sol
+++ contract.sol
@@ -4,12 +4,17 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.0 <0.7.0;
+
+import "@nomiclabs/buidler/console.sol";
+
 contract SimpleStorage {
     uint storedData;
     function set(uint data) public {
+        console.log(msg.sender);
         storedData = data;
     }
     function get() public view returns (uint) {
+        console.log(msg.sender);
         return storedData;
     }
 }
